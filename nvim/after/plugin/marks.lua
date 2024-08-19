b10d29c25a2b0ad8ae9e0eece0ba760b26f56f8b
Lukas Hoffleit <lukas.hoffleit.xd@renesas.com>
2024-08-19 11:49:30 +0200

    added marks

---
+++ nvim/after/plugin/marks.lua
@@ -0,0 +1,8 @@
+require'marks'.setup {
+  --default_mappings = true,
+  cyclic = true,
+  refresh_interval = 250,
+  sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+  excluded_filetypes = {},
+  excluded_buftypes = {},
+}
