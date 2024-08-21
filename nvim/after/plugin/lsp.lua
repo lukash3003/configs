b7a6adcc2cb77547f42333cfbe39355d3f6ef6b0
Lukas Hoffleit <lukas.hoffleit.xd@renesas.com>
2024-08-21 15:36:15 +0200

    Added DiagnosticToggle

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -52,3 +52,15 @@ cmp.setup({
     end,
   },
 })
+
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
+
+
