c2c0a22394397bbc632d321bd039b0dc62b3fe18
lukash3003 <lukas.hoffleit03@gmail.com>
2024-10-11 13:44:10 +0200

    Matlab lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -10,14 +10,24 @@ end)
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {},
-  handlers = {
+ensure_installed = {},
+handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
     end,
-  },
+    matlab_ls = function()
+        require('lspconfig').matlab_ls.setup({
+            filetypes = {"matlab"},
+            settings = {
+                matlab = {
+                    installPath = "/opt/matlab/R2023a/"
+                },
+            },
+            single_file_support = true
+        })
+    end,
+},
 })
-
 require ("lspconfig").verible.setup({})
 
 
